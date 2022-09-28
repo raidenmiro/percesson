0b58ef1f103857347a4a4db3cd6e267395393017
lordszn3 <robertkuzhin@gmail.com>
2022-09-28 17:58:41 +0500

    feat: add unsplash service
    * Add env service
    
    * Established joi

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,22 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import * as env from 'env-var'
 import { AppModule } from './modules/app/app.module'
 
 const logger = new Logger('Application')
 const globalPrefix = 'api'
 
 async function bootstrap() {
-  const port = env.get('PORT').default(3333).asString()
-
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix(globalPrefix)
 
-  await app.listen(port)
-  logger.log(`🚀 Application is running on: http://localhost:${port}`)
+  await app.listen(3333)
+  logger.log(`🚀 Application is running on: http://localhost:3333`)
 }
 
-bootstrap()
+try {
+  bootstrap().catch(err => {
+    logger.error(err, err.stack)
+  })
+} catch (err) {
+  logger.error(err, err.stack)
+}
