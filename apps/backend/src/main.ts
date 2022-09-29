5c168e22f71af2599a365684479b923cf44f8abf
lordszn3 <robertkuzhin@gmail.com>
2022-09-29 13:32:31 +0500

    feat: add swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,27 @@
-import { Logger } from '@nestjs/common'
+import { INestApplication, Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 const logger = new Logger('Application')
 const globalPrefix = 'api'
 
+function setupSwaggerDocs(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Percesso-api')
+    .setDescription('Sync your stuff')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix(globalPrefix)
+
+  setupSwaggerDocs(app)
 
   await app.listen(3333)
   logger.log(`🚀 Application is running on: http://localhost:3333`)
