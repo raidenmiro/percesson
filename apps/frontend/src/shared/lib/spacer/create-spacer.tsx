f52a3769fb8986ec35ed4e6a300a5371525b487e
lordszn3 <robertkuzhin@gmail.com>
2022-10-13 15:41:39 +0500

    fix: add `spacer` helper

---
+++ apps/frontend/src/shared/lib/spacer/create-spacer.tsx
@@ -0,0 +1,66 @@
+import { Dynamic } from 'solid-js/web'
+import { normalizeWithUnit } from './utils'
+
+/*
+ *  const Spacer = createSpacer({
+ *    node: 'span',
+ *    config: {
+ *      factor: 4,
+ *      precision: 1,
+ *      measuringUnit: 'px'
+ *    },
+ *  })
+ *
+ *  use:
+ *  <Spacer h={3} /> // 12px
+ * */
+export interface SpacerConfig {
+  factor: number
+  measuringUnit: 'px' | 'rem'
+}
+
+export const createSpacer = <TElementTag extends HTMLElementTagNameMap, TNode extends keyof TElementTag>({
+  config,
+  node,
+}: {
+  config: { factor?: number; precision?: number; divisor?: number; measuringUnit?: string }
+  node?: TNode
+}) => {
+  const defaultConfig = {
+    factor: 4,
+    measuringUnit: 'px',
+  }
+
+  const settings = Object.assign(defaultConfig, config)
+  const toFormat = normalizeWithUnit(settings.measuringUnit)
+  const cache = new Map()
+
+  const getNormalizedSpace = (spacing: number) => {
+    if (cache.has(spacing)) {
+      return cache.get(spacing)
+    }
+
+    const size = toFormat(settings.factor * spacing)
+    cache.set(spacing, size)
+
+    return size
+  }
+
+  return function (props: { h?: number; w?: number; inline?: boolean }) {
+    const showWithInline = () => props.inline
+
+    return (
+      <Dynamic
+        classList={{
+          block: !showWithInline(),
+          inline: showWithInline(),
+        }}
+        style={{
+          width: getNormalizedSpace(props.w ?? 1),
+          height: getNormalizedSpace(props.h ?? 1),
+        }}
+        component={node ?? 'div'}
+      />
+    )
+  }
+}
