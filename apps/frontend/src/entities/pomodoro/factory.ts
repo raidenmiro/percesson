48351d9a4e46fda3c344a774502910393bf06f4b
lordszn3 <robertkuzhin@gmail.com>
2022-11-09 21:59:19 +0500

    feat: add base factory widget

---
+++ apps/frontend/src/entities/pomodoro/factory.ts
@@ -0,0 +1,7 @@
+import { createViewWidget } from '../../shared/lib/widget'
+import { pomodoroWidget } from './model'
+
+export const PomodoroFactory = createViewWidget({
+  connector: pomodoroWidget,
+  plugins: [],
+})
