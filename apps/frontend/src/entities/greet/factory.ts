bf5ef62451c868441b3b9ba9e87e968d11bfb21f
lordszn3 <robertkuzhin@gmail.com>
2022-11-12 16:29:43 +0500

    fix: typo

--- apps/frontend/src/entities/greet/factory.ts
+++
@@ -1,8 +0,0 @@
-import { createViewWidget } from '../../shared/lib/widget'
-import { greetWidget } from './model'
-import { EaseOut } from './plugins/ease-out'
-
-export const GreetFactory = createViewWidget({
-  connector: greetWidget,
-  plugins: [EaseOut({ duration: [2250, 1000], loop: false })],
-})
