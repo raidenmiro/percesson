dfa0bb0acb1b036bbbad38bafaf8597aaa815994
lordszn3 <robertkuzhin@gmail.com>
2022-11-12 14:40:23 +0500

    chore: add wrapper for greet

---
+++ apps/frontend/src/entities/greet/factory.tsx
@@ -0,0 +1,10 @@
+import { createViewWidget } from '../../shared/lib/widget'
+import { SettingsWrapper } from '../../shared/templates/settings-wrapper'
+import { greetWidget } from './model'
+import { EaseOut } from './plugins/ease-out'
+
+export const GreetFactory = createViewWidget({
+  connector: greetWidget,
+  plugins: [EaseOut({ duration: [2250, 1000], loop: true })],
+  extend: widget => <SettingsWrapper>{widget}</SettingsWrapper>,
+})
