a83ae70627edbf057445e64e7c5281d71754d10e
lordszn3 <robertkuzhin@gmail.com>
2022-11-14 13:44:04 +0500

    chore: add use state from model

--- apps/frontend/src/entities/greet/model.ts
+++ apps/frontend/src/entities/greet/model.ts
@@ -1,5 +1,7 @@
-import { createStore } from 'effector'
+import { createEvent, createStore } from 'effector'
 import { createWidget } from '../../shared/lib/widget'
 
 export const greetWidget = createWidget()
-export const $greetMessage = createStore('Welcome!')
+
+export const textChanged = createEvent<string>()
+export const $text = createStore('Good morning, set the mood!').on(textChanged, (_, text) => text)
--- apps/frontend/src/entities/greet/variants/primary.tsx
+++ apps/frontend/src/entities/greet/variants/primary.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable solid/no-innerhtml */
+import { useUnit } from 'effector-solid'
 import { GreetFactory } from '../factory'
+import { $text } from '../model'
 
-export const Primary = (props: { text: string }) => {
+export const Primary = () => {
+  const text = useUnit($text)
+
   return (
     <GreetFactory
       as="h1"
       class="absolute top-5 text-white text-7xl"
-      innerHTML={props.text
+      innerHTML={text()
         .split('')
         .map(letter => `<span class='letter'>${letter}</span>`)
         .join('')}
