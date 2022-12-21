6f83ba940c17c0c815a12f9a9526d82a414f0973
lordszn3 <robertkuzhin@gmail.com>
2022-12-21 17:49:27 +0500

    chore: add helper for computed size

--- apps/extension/src/features/snap-to-grid/lib.ts
+++ apps/extension/src/features/snap-to-grid/lib.ts
@@ -1,49 +1,100 @@
-import { Transformer } from '@thisbeyond/solid-dnd'
+export function rectCache<Node extends HTMLElement, ID extends string | number>() {
+  const cache: Record<ID, DOMRect> = {} as Record<ID, DOMRect>
 
-export function snapToGrid({
-  transform,
-  gridSize,
-}: {
-  transform: ReturnType<Transformer['callback']>
-  gridSize: number
-}) {
-  return {
-    x: Math.ceil(transform.x / gridSize) * gridSize,
-    y: Math.ceil(transform.y / gridSize) * gridSize,
+  return function (node: Node, id: ID) {
+    if (id in cache) return cache[id]
+
+    const rect = getClientRect(node, { ignoreTransform: true })
+    cache[id] = rect
+
+    return rect
   }
 }
 
-export function getBounds() {
-  const { innerHeight, innerWidth } = window
+export function getWindowClientRect() {
+  const width = window.innerWidth
+  const height = window.innerHeight
 
   return {
     top: 0,
     left: 0,
-    right: innerWidth,
-    bottom: innerHeight,
-    width: innerWidth,
-    height: innerHeight,
+    right: width,
+    bottom: height,
+    width,
+    height,
   }
 }
 
-export function restrictToBoundingRect(
-  transform: ReturnType<Transformer['callback']>,
-  config: { elementRect: DOMRect; boundsRect: Omit<DOMRect, 'x' | 'y' | 'toJSON'> },
-) {
-  const { elementRect: rect, boundsRect: boundingRect } = config
-  const currentPosition = { ...transform }
+export function getClientRect(element: Element, options: { ignoreTransform: boolean }): DOMRect {
+  let rect: DOMRect = element.getBoundingClientRect()
 
-  if (rect.top + transform.y <= boundingRect.top) {
-    currentPosition.y = boundingRect.top - rect.top
-  } else if (rect.bottom + transform.y >= boundingRect.top + boundingRect.height) {
-    currentPosition.y = boundingRect.top + boundingRect.height - rect.bottom
+  if (options.ignoreTransform) {
+    const { getComputedStyle } = window
+    const { transform, transformOrigin } = getComputedStyle(element)
+
+    if (transform) {
+      rect = inverseTransform(rect, transform, transformOrigin) as DOMRect
+    }
   }
 
-  if (rect.left + transform.x <= boundingRect.left) {
-    currentPosition.x = boundingRect.left - rect.left
-  } else if (rect.right + transform.x >= boundingRect.left + boundingRect.width) {
-    currentPosition.x = boundingRect.left + boundingRect.width - rect.right
+  const { top, left, width, height, bottom, right } = rect
+
+  return {
+    top,
+    left,
+    width,
+    height,
+    bottom,
+    right,
+  } as DOMRect
+}
+
+export function inverseTransform(rect: DOMRect, transform: string, transformOrigin: string) {
+  const parsedTransform = parseTransform(transform)
+
+  if (!parsedTransform) {
+    return rect
   }
 
-  return currentPosition
+  const { scaleX, scaleY, x: translateX, y: translateY } = parsedTransform
+
+  const x = rect.left - translateX - (1 - scaleX) * Number.parseFloat(transformOrigin)
+  const y =
+    rect.top - translateY - (1 - scaleY) * Number.parseFloat(transformOrigin.slice(transformOrigin.indexOf(' ') + 1))
+  const w = scaleX ? rect.width / scaleX : rect.width
+  const h = scaleY ? rect.height / scaleY : rect.height
+
+  return {
+    width: w,
+    height: h,
+    top: y,
+    right: x + w,
+    bottom: y + h,
+    left: x,
+  }
+}
+
+export function parseTransform(transform: string) {
+  if (transform.startsWith('matrix3d(')) {
+    const transformArray = transform.slice(9, -1).split(/, /)
+
+    return {
+      x: Number(transformArray[12]),
+      y: Number(transformArray[13]),
+      scaleX: Number(transformArray[0]),
+      scaleY: Number(transformArray[5]),
+    }
+  }
+  if (transform.startsWith('matrix(')) {
+    const transformArray = transform.slice(7, -1).split(/, /)
+
+    return {
+      x: Number(transformArray[4]),
+      y: Number(transformArray[5]),
+      scaleX: Number(transformArray[0]),
+      scaleY: Number(transformArray[3]),
+    }
+  }
+
+  return null
 }
